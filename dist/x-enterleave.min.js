bacbcc7fd56a5fc8f7bfc31b30fec6873c39fed3
jfroffice <jfroffice@gmail.com>
2014-11-30 23:40:29 +0100

    update version for bower.json

--- dist/x-enterleave.min.js
+++ dist/x-enterleave.min.js
@@ -1,6 +1,6 @@
 /**
  * x-enterleave.js - HTML attribute to add dynamic CSS modifier on classes
- * @version v0.1.5
+ * @version v0.1.6
  * @link https://github.com/jfroffice/x-enterleave.js
  * @license MIT
  */
