18985e3b273bc68293d05b531ac86b4cf42c8579
jfroffice <jfroffice@gmail.com>
2014-12-21 15:46:47 +0100

    update build version

--- dist/x-enterleave.js
+++ dist/x-enterleave.js
@@ -1,6 +1,6 @@
 /**
  * x-enterleave.js - HTML attribute to add dynamic CSS modifier on classes
- * @version v0.1.8
+ * @version v0.1.9
  * @link https://github.com/jfroffice/x-enterleave.js
  * @license MIT
  */
